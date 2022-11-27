742b232f06fba50d760cd2ed74c8ff2557a2d5eb
Eray Sahin <eraysahinmoon@gmail.com>
2022-11-27 09:53:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,4 +12,5 @@ mongoose.connect(process.env.DATABASE_ACCESS, () => console.log('Database connec
 app.use(express.json());
 app.use(cors());
 app.use('/app', routesUrls);
+
 app.listen(4000, () => console.log("Server on"));
