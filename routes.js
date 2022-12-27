ddceb05552b14b42b6fee23e3e8d8b9182cd3a82
Eray Sahin <eraysahinmoon@gmail.com>
2022-12-27 10:46:16 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const singUpTemplateCopy = require('../models/SignUpModels');
 const bcrypt = require('bcrypt');
-
 router.post('/signup', async(request, response) =>{
     const saltPassword =  await bcrypt.genSalt(10);
     const securePassword = await bcrypt.hash(request.body.password, saltPassword);
